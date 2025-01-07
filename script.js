e18ab889261ae5dbc16f902a8471753da8922134
Prantik <prantiknoor@gmail.com>
2025-01-07 14:35:52 +0600

    Move icons in svg files

--- script.js
+++ script.js
@@ -107,22 +107,13 @@ function toggleFullscreen() {
 
 // Update volume icon based on volume level
 function updateVolumeIcon(volume) {
-  const path = volumeBtn.querySelector("path");
+  const volumeIcon = volumeBtn.querySelector("img");
   if (volume === 0 || videoPlayer.muted) {
-    path.setAttribute(
-      "d",
-      "M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"
-    );
+    volumeIcon.src = "icons/volume-muted.svg";
   } else if (volume < 0.5) {
-    path.setAttribute(
-      "d",
-      "M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02z"
-    );
+    volumeIcon.src = "icons/volume-low.svg";
   } else {
-    path.setAttribute(
-      "d",
-      "M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"
-    );
+    volumeIcon.src = "icons/volume-high.svg";
   }
 }
 
@@ -170,7 +161,7 @@ setInterval(() => {
   }
 }, 5000);
 
-videoPlayer.addEventListener("timeupdate", () => {
+videoPlayer.addEventListener("timeupdate", () => {  
   if (videoPlayer.duration) {
     const percentage = (videoPlayer.currentTime / videoPlayer.duration) * 100;
     progress.style.width = `${percentage}%`;
@@ -289,30 +280,27 @@ speedBtn.addEventListener("click", () => {
 fullscreenBtn.addEventListener("click", toggleFullscreen);
 
 document.addEventListener("fullscreenchange", () => {
+  const fullscreenIcon = fullscreenBtn.querySelector("img");
   if (document.fullscreenElement) {
     videoContainer.classList.add("fullscreen");
+    fullscreenIcon.src = "icons/fullscreen-exit.svg";
     showNotification("Fullscreen");
   } else {
     videoContainer.classList.remove("fullscreen");
+    fullscreenIcon.src = "icons/fullscreen.svg";
     controls.style.opacity = "1";
     showNotification("Exit Fullscreen");
   }
 });
 
 videoPlayer.addEventListener("play", () => {
-  playPauseBtn.innerHTML = `
-        <svg width="24" height="24" viewBox="0 0 24 24">
-            <path fill="currentColor" d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
-        </svg>
-    `;
+  const playPauseIcon = playPauseBtn.querySelector("img");
+  playPauseIcon.src = "icons/pause.svg";
 });
 
 videoPlayer.addEventListener("pause", () => {
-  playPauseBtn.innerHTML = `
-        <svg width="24" height="24" viewBox="0 0 24 24">
-            <path fill="currentColor" d="M8 5v14l11-7z"/>
-        </svg>
-    `;
+  const playPauseIcon = playPauseBtn.querySelector("img");
+  playPauseIcon.src = "icons/play.svg";
 });
 
 // Initialize volume icon
